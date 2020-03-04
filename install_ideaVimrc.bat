7e582835ba8efa559c2d38af9d3b59273b9c8b51
quinnpan <quinnpan@tencent.com>
2020-03-04 16:03:47 +0800

    update vsvim cnfig

--- install_ideaVimrc.bat
+++ install_ideaVimrc.bat
@@ -1,2 +1,3 @@
 REM 安装idealvim的配置文件vimrc到用户当前目录下。ideal IDE需要安装idealvim这个插件
 copy /b idea\idea.vimrc "%userprofile%\.ideavimrc"
+REM mklink /h idea\idea.vimrc "%userprofile%\.ideavimrc"
--- install_vsvimrc.bat
+++ install_vsvimrc.bat
@@ -1,2 +1,3 @@
 REM 安装vsvim的配置文件vimrc到用户当前目录下。vs需要安装vsvim这个插件
 copy /b vs\_vsvimrc "%userprofile%\_vsvimrc"
+REM mklink /h  "%userprofile%\_vsvimrc" vs\_vsvimrc
