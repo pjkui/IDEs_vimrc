9de81f84abe427eec979c737b788e4ac02b40251
quinnpan <quinnpan@tencent.com>
2022-11-01 19:19:25 +0800

    add vimrc for va

--- install_vsvimrc_va.bat
+++ install_vsvimrc_va.bat
@@ -7,5 +7,6 @@ chcp 65001
 
 REM 安装vsvim的配置文件vimrc到用户当前目录下。vs需要安装vsvim这个插件
 echo "把用户目录下的vsvimrc复制回来"
-copy /b  "%userprofile%\_vsvimrc" vs\_vsvimrc
+copy /b  "%userprofile%\_vsvimrc" vs\_vsvimrc_va
 REM mklink /h  "%userprofile%\_vsvimrc" vs\_vsvimrc
+pause
