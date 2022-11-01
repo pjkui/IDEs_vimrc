3540be372059e0b1d1bc5b24b064d4dc858aba9e
quinnpan <quinnpan@tencent.com>
2022-11-01 19:24:28 +0800

    fixed some bug

--- install_vsvimrc_va.bat
+++ install_vsvimrc_va.bat
@@ -1,12 +1,12 @@
 echo off
 GOTO comment
 把VA相关的快捷的安装到vs的配置目录
+REM 安装vsvim的配置文件vimrc到用户当前目录下。vs需要安装vsvim这个插件
 :comment
 
 chcp 65001
 
-REM 安装vsvim的配置文件vimrc到用户当前目录下。vs需要安装vsvim这个插件
-echo "把用户目录下的vsvimrc复制回来"
-copy /b  "%userprofile%\_vsvimrc" vs\_vsvimrc_va
+echo "把vs/vsvimrc_va复制到用户的目录下%userprofile%\_vsvimrc"
+copy /b vs\_vsvimrc_va "%userprofile%\_vsvimrc" 
 REM mklink /h  "%userprofile%\_vsvimrc" vs\_vsvimrc
 pause
